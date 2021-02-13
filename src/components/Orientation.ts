01faeb3ef78e9e4d7f83d4f0694fde411e3dcbe9
Phuoc Nguyen <me@phuoc.ng>
2021-02-13 17:58:52 +0700

    Rotate browser frame

---
+++ src/components/Orientation.ts
@@ -0,0 +1,12 @@
+/**
+ * A collection of patterns to create a responsive web page
+ * https://responsive.page
+ * (c) 2021 Nguyen Huu Phuoc (https://twitter.com/nghuuphuoc)
+ */
+
+enum Orientation {
+    Landscape,
+    Portrait,
+}
+
+export default Orientation;
