e51f08c433d502be0d92c1a086b78a553218eddf
Phuoc Nguyen <me@phuoc.ng>
2021-02-13 11:06:59 +0700

    Update container size when it's resized

--- src/components/demo-viewer/demo-viewer.tsx
+++ src/components/demo-viewer/demo-viewer.tsx
@@ -73,14 +73,6 @@ export class DemoViewer {
 
         this.frameDemoEle.style.removeProperty('height');
         this.frameDemoEle.style.removeProperty('width');
-
-        // const scale = this.calculateScale(width, height);
-
-        // this.frameDemoEle.style.height = `${height}px`;
-        // this.frameDemoEle.style.width = `${width}px`;
-        // this.frameDemoEle.style.transform = scale === 1
-        //         ? 'scale(1)'
-        //         : `translate(${width * (scale - 1) / 2}px, ${height * (scale - 1) / 2}px) scale(${scale})`;
     }
 
     handleDidResize = (e: CustomEvent<ResizeEvent>) => {
@@ -91,27 +83,20 @@ export class DemoViewer {
     componentDidLoad() {
         const padding = parseInt(window.getComputedStyle(this.frameContainer).padding);
         const { height, width } = this.frameContainer.getBoundingClientRect();
-        const containerWHeight = height - padding * 2;
+        const containerHeight = height - padding * 2;
         const containerWidth = width - padding * 2;
 
-        this.frameContainerHeight = containerWHeight;
+        this.frameContainerHeight = containerHeight;
         this.frameContainerWidth = containerWidth;
 
-        this.switchTo(containerWidth, containerWHeight);
+        this.switchTo(containerWidth, containerHeight);
 
         // Automatically update the size of container
         this.resizeObserver = new ResizeObserver(entries => {
             entries.forEach(entry => {
-                // const { height, width } = entry.contentRect;
-
-                // this.frameContainerWidth = width;
-                // this.frameContainerHeight = height;
-
-                // this.frameContainer.style.width = `${width}px`;
-                // this.frameContainer.style.height = `${height}px`;
-
-                // console.log(height, width);
-                // this.switchTo(width, height);
+                const { height, width } = entry.contentRect;
+                this.frameContainerHeight = height;
+                this.frameContainerWidth = width;
             });
         });
         this.resizeObserver.observe(this.frameContainer);
