89706c3df495ec24e8261bc88478fb1195bb3f96
Phuoc Nguyen <me@phuoc.ng>
2021-02-14 13:48:44 +0700

    Move ResizeObserver type definitions

--- src/components/demo-viewer/demo-viewer.tsx
+++ src/components/demo-viewer/demo-viewer.tsx
@@ -9,42 +9,10 @@ import { Component, Prop, State, h } from '@stencil/core';
 import slugify from '../../utils/slugify';
 import unslugify from '../../utils/unslugify';
 import { ResizeEvent } from '../resize-able/resize-able';
+import ResizeObserver from './ResizeObserver';
 import Orientation from '../Orientation';
 import { PATTERNS } from '../Patterns';
 import { ScreenSize } from '../ScreenSize';
-
-interface DOMRectReadOnly {
-    readonly x: number;
-    readonly y: number;
-    readonly width: number;
-    readonly height: number;
-    readonly top: number;
-    readonly right: number;
-    readonly bottom: number;
-    readonly left: number;
-}
-
-declare global {    
-    interface ResizeObserverCallback {
-        (entries: ResizeObserverEntry[], observer: ResizeObserver): void
-    }
-
-    interface ResizeObserverEntry {
-        readonly target: Element;
-        readonly contentRect: DOMRectReadOnly;
-    }
-
-    interface ResizeObserver {
-        observe(target: Element): void;
-        unobserve(target: Element): void;
-        disconnect(): void;
-    }
-}
-
-declare var ResizeObserver: {
-    prototype: ResizeObserver;
-    new(callback: ResizeObserverCallback): ResizeObserver;
-}
 
 @Component({
     tag: 'demo-viewer',
