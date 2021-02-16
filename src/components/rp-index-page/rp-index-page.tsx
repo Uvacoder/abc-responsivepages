66709df435401b1a642be9142b15386146ca88fc
Phuoc Nguyen <me@phuoc.ng>
2021-02-16 22:39:09 +0700

    Introduce features

--- src/components/rp-index-page/rp-index-page.tsx
+++ src/components/rp-index-page/rp-index-page.tsx
@@ -32,6 +32,47 @@ export class RpIndexPage {
                             <a href="https://github.com/phuoc-ng/responsive-page" target="_blank">GitHub</a>
                         </div>
                     </div>
+
+                    <div class="rp-index-page__navigate">
+                        <rp-tooltip tip="Navigate between patterns" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__rotate">
+                        <rp-tooltip tip="Rotate screen" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__source">
+                        <rp-tooltip tip="View pattern implementation" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__screen">
+                        <rp-tooltip tip="Switch screen" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__screens">
+                        <rp-tooltip tip="Choose from screens" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__resize--r">
+                        <rp-tooltip tip="Resize the screen" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__resize--b">
+                        <rp-tooltip tip="Resize the screen" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
+                    <div class="rp-index-page__resize--rb">
+                        <rp-tooltip tip="Resize the screen" position="bottom">
+                            <rp-pulse />
+                        </rp-tooltip>
+                    </div>
                 </div>
             </rp-layout>
         );
